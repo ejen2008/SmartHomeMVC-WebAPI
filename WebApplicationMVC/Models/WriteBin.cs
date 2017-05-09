9c725c5fc3235384409d17486640bdf7cf0bbb32
Евгений <ejen2008@mail.ru>
2017-05-09 22:07:07 +0300

    add project

---
+++ WebApplicationMVC/Models/WriteBin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Web;
+
+namespace WebApplicationMVC.Models
+{
+    public class WriteBin : IWriteble
+    {
+        public void Write(DeviceDataView data, string nameFile)
+        {
+            BinaryFormatter binFormatter = new BinaryFormatter();
+            using (FileStream deviceData = new FileStream(nameFile, FileMode.OpenOrCreate))
+            {
+                binFormatter.Serialize(deviceData, data);
+            }
+        }
+    }
+}
